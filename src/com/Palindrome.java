828ef731085bb1aa0f70c8eef93b891f94a613d6
Magda Fairfax <magda.fairfax@gmail.com>
2020-06-20 08:50:10 +0100

    Remove out folder.

--- src/com/Palindrome.java
+++ src/com/Palindrome.java
@@ -1,12 +1,19 @@
 package com;
 
-public class Palidrome {
+public class Palindrome {
 
     public boolean isPalidromeUsingStringBuffer(String givenString) {
+        boolean isPalindrome = true;
         String clean = givenString.replaceAll("\\s+", "").toLowerCase();
-
         StringBuffer plain = new StringBuffer(clean);
         StringBuffer reverse = plain.reverse();
-        return (reverse.toString()).equals(plain);
+        isPalindrome = (reverse.toString()).equals(clean);
+
+        return isPalindrome;
+    }
+
+    public static  void main (String[] args){
+        Palindrome palindrome = new Palindrome();
+        palindrome.isPalidromeUsingStringBuffer("Was it a rat I saw");
     }
 }
--- src/test/PalindromeTest.java
+++ src/test/PalindromeTest.java
@@ -1,4 +1,18 @@
 package test;
 
+import com.Palindrome;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PalindromeTest {
+    Palindrome palindrome = new Palindrome();
+
+    @Test
+    public void checkPalidrome() {
+        assertEquals(true, palindrome.isPalidromeUsingStringBuffer("Was it a rat I saw"));
+        assertEquals(true,palindrome.isPalidromeUsingStringBuffer(""));
+        assertEquals(false,palindrome.isPalidromeUsingStringBuffer("Was it a rat I saw?"));
+
+    }
 }
